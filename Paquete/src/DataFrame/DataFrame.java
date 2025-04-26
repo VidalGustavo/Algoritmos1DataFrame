e90446d3c82a442239092d2c4e26c256a45136c1
mbacalhau <150526059+mbacalhau@users.noreply.github.com>
2025-04-26 17:27:52 -0300

    clase DataFrame e inicialización del paquete

---
+++ Paquete/src/DataFrame/DataFrame.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class DataFrame {
+    private ArrayList <Rows> rows ;
+    private int numRow;
+    private int numCol ;
+    private ArrayList<Column> columns;
+
+
+    public DataFrame(ArrayList)  {
+        // Si inicializa vacio asume que es una columna
+        
+        
+        this.numCol = numCol;
+        this.columns = columns;
+    }
+    public DataFrame(ArrayList <ArrayList <Object>> ){
+
+            }
+    
+
+    public DataFrame (String) {
+
+    }
+
+    
+
+
+}
