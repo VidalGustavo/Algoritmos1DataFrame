952fe1311ed685c98ec63daf267e2ddd86b8bd8c
mbacalhau <150526059+mbacalhau@users.noreply.github.com>
2025-05-20 14:55:38 -0300

    retoqué celda tipo T, cree tipo String

---
+++ Dataframe/src/celda/CeldaString.java
@@ -0,0 +1,13 @@
+package Celda;
+
+public class CeldaString extends Celda<String> {
+
+    public CeldaString(String value) {
+        super(value);
+    }
+
+    // Puedes agregar métodos específicos para String si lo necesitas
+    public String getValue() {
+        return super.getValue();
+    }
+}
