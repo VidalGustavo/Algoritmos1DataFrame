c89c8a715ba90e698cd7caf68369f7f37ccc00f8
mbacalhau <150526059+mbacalhau@users.noreply.github.com>
2025-05-30 15:12:14 -0300

    cambios

---
+++ Dataframe/src/celda/CeldaNumber.java
@@ -0,0 +1,13 @@
+package celda;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CeldaNumber extends Celda<Number> {
+
+    public CeldaNumber(Number value) {
+        super(value);
+    }
+
+    
+}
